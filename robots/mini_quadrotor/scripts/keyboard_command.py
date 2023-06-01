2a469e7a97f053d185b2a107465b1355968cd40b
Shotaro Itahara <itahara-shotaro@ynl.t.u-tokyo.ac.jp>
2023-06-01 21:22:21 +0900

    implemented scrpits for launching simulation with and  controlling two quadrotors

---
+++ robots/mini_quadrotor/scripts/keyboard_command.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+import rospy
+
+from std_msgs.msg import Empty
+from std_msgs.msg import Int8
+from std_msgs.msg import UInt16
+from std_msgs.msg import UInt8
+import rosgraph
+
+import sys, select, termios, tty
+
+def getKey():
+        tty.setraw(sys.stdin.fileno())
+        select.select([sys.stdin], [], [], 0)
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        return key
+
+if __name__=="__main__":
+        settings = termios.tcgetattr(sys.stdin)
+        rospy.init_node("keyboard_command")
+        #robot_ns = rospy.get_param("~robot_ns", "");
+        robot_ns1="quadrotor1"
+        robot_ns2="quadrotor2"
+
+
+        ns1 = robot_ns1 + "/teleop_command"
+        land_pub1 = rospy.Publisher(ns1 + '/land', Empty, queue_size=1)
+        halt_pub1 = rospy.Publisher(ns1 + '/halt', Empty, queue_size=1)
+        start_pub1 = rospy.Publisher(ns1 + '/start', Empty, queue_size=1)
+        takeoff_pub1 = rospy.Publisher(ns1 + '/takeoff', Empty, queue_size=1)
+        force_landing_pub1 = rospy.Publisher(ns1 + '/force_landing', Empty, queue_size=1)
+        ctrl_mode_pub1 = rospy.Publisher(ns1 + '/ctrl_mode', Int8, queue_size=1)
+        motion_start_pub1 = rospy.Publisher('task_start', Empty, queue_size=1)
+
+        ns2 = robot_ns2 + "/teleop_command"
+        land_pub2 = rospy.Publisher(ns2 + '/land', Empty, queue_size=1)
+        halt_pub2 = rospy.Publisher(ns2 + '/halt', Empty, queue_size=1)
+        start_pub2 = rospy.Publisher(ns2 + '/start', Empty, queue_size=1)
+        takeoff_pub2 = rospy.Publisher(ns2 + '/takeoff', Empty, queue_size=1)
+        force_landing_pub2 = rospy.Publisher(ns2 + '/force_landing', Empty, queue_size=1)
+        ctrl_mode_pub2 = rospy.Publisher(ns2 + '/ctrl_mode', Int8, queue_size=1)
+        motion_start_pub2 = rospy.Publisher('task_start', Empty, queue_size=1)
+
+
+
+        #the way to write publisher in python
+        comm=Int8()
+        gain=UInt16()
+        try:
+                while(True):
+                        key = getKey()
+                        print("the key value is {}".format(ord(key)))
+                        # takeoff and landing
+                        if key == 'l':
+                                land_pub1.publish(Empty())
+                                land_pub2.publish(Empty())
+                                #for hydra joints
+                        if key == 'r':
+                                start_pub1.publish(Empty())
+                                start_pub2.publish(Empty())
+                                #for hydra joints
+                        if key == 'h':
+                                halt_pub.publish(Empty())
+                                 #for hydra joints
+                        if key == 'f':
+                                force_landing_pub.publish(Empty())
+                        if key == 't':
+                                takeoff_pub1.publish(Empty())
+                                takeoff_pub2.publish(Empty())
+                        if key == 'u':
+                                stair_pub.publish(Empty())
+                        if key == 'x':
+                                motion_start_pub.publish()
+                        if key == 'v':
+                                comm.data = 1
+                                ctrl_mode_pub.publish(comm)
+                        if key == 'p':
+                                comm.data = 0
+                                ctrl_mode_pub.publish(comm)
+                        if key == '\x03':
+                                break
+                        rospy.sleep(0.001)
+
+        except Exception as e:
+                print(repr(e))
+        finally:
+                termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+
+#todo: set original hovering location as rosparam once /quadrotor/flight_state becomes 5
---
+++ robots/mini_quadrotor/scripts/task_command.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python3.8
+
+# moton/に入ったjsonに記載された位置に移動する
+# 使用するjsonファイルは引数で受け取る
+
+# 操作対象のロボット名は quadrotor1 と quadrotor2, 目標位置(速度)を伝える先は /<ロボット名>/uav/nav
+# メッセージ形式はaerial_robot_msgs/FlightNav
+import rospy
+import sys
+import json
+import math
+from numpy import deg2rad, arccos
+from aerial_robot_msgs.msg import FlightNav
+from geometry_msgs.msg import PoseStamped, Point
+
+
+def task_command(name1="quadrotor1", name2="quadrotor2", task_name="motion1"):
+
+    file_name=task_name+".json"
+
+    with open(f"motion/{file_name}",'r') as file:
+        motion_data=json.loads(file.read())["movement"]
+
+    # prepare publisher
+    rospy.init_node("task_command")
+    pub1 = rospy.Publisher(f'/{name1}/uav/nav',FlightNav, queue_size=10)
+    pub2 = rospy.Publisher(f'/{name2}/uav/nav',FlightNav, queue_size=10)
+    rate=rospy.Rate(100)
+
+    # get starting position/yaw
+    pos1=[]
+    pos2=[]
+
+    yaw1=[]
+    yaw2=[]
+    def cb1(data):
+        pos1.append(data.pose.position)
+        yaw1.append(2*arccos(data.pose.orientation.w))
+
+    def cb2(data):
+        pos2.append(data.pose.position)
+        yaw2.append(2*arccos(data.pose.orientation.w))
+
+    def retrieve_pos():
+        sub1=rospy.Subscriber(f'/{name1}/mocap/pose',PoseStamped,cb1)
+        sub2=rospy.Subscriber(f'/{name2}/mocap/pose',PoseStamped,cb2)
+        while not rospy.is_shutdown():
+            if len(pos1)>=10 and len(pos2)>=10:
+                sub1.unregister()
+                sub2.unregister()
+                break
+        rospy.sleep(0.1)
+
+    def mean_position(point_array):
+        mean_point = Point()
+        num_points = len(point_array)
+
+        if num_points == 0:
+            return mean_point
+
+        sum_x = 0.0
+        sum_y = 0.0
+        sum_z = 0.0
+
+        for point in point_array:
+            sum_x += point.x
+            sum_y += point.y
+            sum_z += point.z
+
+        mean_point.x = sum_x / num_points
+        mean_point.y = sum_y / num_points
+        mean_point.z = sum_z / num_points
+
+        return mean_point
+
+    retrieve_pos()
+
+    pos1_orig=mean_position(pos1)
+    pos2_orig=mean_position(pos2)
+
+    yaw1_orig=sum(yaw1)/len(yaw1)
+    yaw2_orig=sum(yaw2)/len(yaw2)
+
+    # execute described motions
+
+    for command in motion_data:
+        print(command)
+        qr1=command["qr1"]
+        qr2=command["qr2"]
+        duration=command["duration"]
+
+        # generate message
+        
+        qr1_msg=FlightNav()
+        qr2_msg=FlightNav()
+
+        qr1_tgt=[pos1_orig.x+qr1["x"], pos1_orig.y+qr1["y"], pos1_orig.z+qr1["z"], yaw1_orig+deg2rad(qr1["yaw"]) ]
+        qr2_tgt=[pos2_orig.x+qr2["x"], pos2_orig.y+qr2["y"], pos2_orig.z+qr2["z"], yaw2_orig+deg2rad(qr2["yaw"]) ]
+
+        qr1_msg.pos_xy_nav_mode=2
+        qr1_msg.yaw_nav_mode=2
+        qr1_msg.pos_z_nav_mode=2
+        [qr1_msg.target_pos_x, qr1_msg.target_pos_y, qr1_msg.target_pos_z, qr1_msg.target_yaw]=qr1_tgt
+
+        qr2_msg.pos_xy_nav_mode=2
+        qr2_msg.yaw_nav_mode=2
+        qr2_msg.pos_z_nav_mode=2
+        [qr2_msg.target_pos_x, qr2_msg.target_pos_y, qr2_msg.target_pos_z, qr2_msg.target_yaw]=qr2_tgt
+
+        #publish message for duration
+
+        time_delta=rospy.Duration(duration)
+        end_time=rospy.get_rostime()+time_delta
+
+        while not rospy.is_shutdown() and rospy.get_rostime()<end_time:
+            pub1.publish(qr1_msg)
+            pub2.publish(qr2_msg)
+            rate.sleep()
+
+    rospy.signal_shutdown("Movement finished")
+
+if __name__==__main__:
+    task_command()
