36b2293944c904fd8fc3ecc2395b93a793dd1161
Shotaro Itahara <itahara-shotaro@ynl.t.u-tokyo.ac.jp>
2026-08-31 17:44:51 +0900

    feat: make gazebo terminate faster with a sim-only script

---
+++ aerial_robot_base/scripts/gazebo_killer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys, signal, subprocess, time
+
+
+timeout_before_kill = 0.1  # [s]
+timeout_after_kill = 0.1  # [s]
+
+
+def signal_handler(sig, frame):
+    time.sleep(timeout_before_kill)
+    subprocess.call("killall -q gzclient & killall -q gzserver", shell=True)
+    time.sleep(timeout_after_kill)
+    subprocess.call("killall -9 -q gzclient & killall -9 -q gzserver", shell=True)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    input("")
